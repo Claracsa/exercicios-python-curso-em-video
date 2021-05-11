ad6d55d318eeb9c9e7f595fc39bd15ae0a3173e9
Clara da Silveira Arantes <claracsa1@gmail.com>
2021-05-11 10:25:38 -0300

    utilizando módulos
    sorteando um item na lista

---
+++ Exercícios/ex019.py
@@ -0,0 +1,8 @@
+import random
+a = str(input('Primeiro aluno: '))
+b = str(input('Segundo aluno: '))
+c = str(input('Terceiro aluno: '))
+d = str(input('Quarto aluno: '))
+lista = [a, b, c, d]
+aleatório = random.choice(lista)
+print('O aluno escolhido foi {}.'.format(aleatório))
